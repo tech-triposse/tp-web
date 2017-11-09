1d1c486f06b5dadd0966539703f9538e3d186ce5
Akhil Tayal <akhiltayal2818@gmail.com>
2017-11-10 00:07:38 +0530

    db url changed

---
+++ config/db.config.js
@@ -0,0 +1,12 @@
+var mongoClient = require('mongodb').MongoClient;
+
+var localUrl = "mongodb://localhost:27017/blogDb";
+
+var url = "mongodb://akhiltayal:'dutta@123'@ds255265.mlab.com:55265/triposse";
+
+module.exports.connect = function connect(callback){
+  mongoClient.connect(url, function(err, conn){
+    module.exports.db = conn;
+    callback(err);
+  });
+}
